023164b953fa0aed23c27515a8ec5419e2d88578
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-05-22 15:38:20 +0100

    add express

--- test/SearchBar.test.ts
+++ test/SearchBar.test.ts
@@ -1,8 +1,19 @@
 import {AsyncTest, Expect, Test, TestFixture} from "alsatian";
+import { debug } from "util";
 import { SearchBar } from "../src/components/Search/SearchBar";
+// tslint:disable-next-line:no-var-requires
+const MockFetch = require("mock-fetch-api");
 
 @TestFixture()
 export class SearchBarTests {
+    @Test()
+    public async handles404() {
+        MockFetch.when("GET", "https://api.github.com/").respondWith(404);
+
+        const response = await fetch("https://api.github.com/");
+        Expect(response.status).toBe(0);
+    }
+
     @Test()
     public shouldBeTwo() {
         Expect(1 + 2).toBe(3);
