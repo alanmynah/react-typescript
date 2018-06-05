59b32d3840d449a1227e9b52df9dee8d618ca1db
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-06-05 12:13:41 +0100

    add blob function to test end to end

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,15 @@
+import * as bodyParser from "body-parser";
 import * as Express from "express";
 import * as path from "path";
 import { router } from "./api";
 
 const server = Express();
 
+server.use(bodyParser.json());
+
 server.use("/", Express.static(path.resolve("dist")));
 server.use("/node_modules", Express.static(path.resolve("node_modules")));
-server.use(router);
+server.use("/api", router);
 
 server.get("/", (req, res) => {
     res.sendFile(path.resolve("dist/index.html"));
