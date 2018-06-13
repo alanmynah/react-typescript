17e0324a7ec82914541a509021667cc9c9c7f297
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-06-13 08:52:09 +0100

    wow, just this is needed to enable FE routing

--- src/server/server.ts
+++ src/server/server.ts
@@ -13,7 +13,7 @@ server.use("/", Express.static(path.resolve("dist")));
 server.use("/node_modules", Express.static(path.resolve("node_modules")));
 server.use("/api", router);
 
-server.get("/", (req, res) => {
+server.get("*", (req, res) => {
     res.sendFile(path.resolve("dist/index.html"));
 });
 
