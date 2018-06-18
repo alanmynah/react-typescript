e9a5dc63a0c193618fc4afa7a030fc67d2193410
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-06-18 09:14:46 +0100

    filters work and somewhat refactored

--- src/public/components/Camera/Camera.tsx
+++ src/public/components/Camera/Camera.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Button, Grid, Image, List, Segment } from "semantic-ui-react";
+import { Button, Grid, Segment, Form } from "semantic-ui-react";
 import axios from "axios";
-import { PhotoBlob, JsonBlobData, FaceImage } from "../../../server/models";
+import { PhotoBlob, FaceImage } from "../../../server/models";
+import { colourEffect } from "./colourFilters";
 
 interface CameraProps {
     width: number;
@@ -15,6 +15,7 @@ interface CameraState {
     width: number;
     height: number;
     stream: MediaStream;
+    originalImage: any;
     facingMode: string;
     constraints: MediaTrackSupportedConstraints;
     devices: MediaDeviceInfo[];
@@ -27,19 +28,24 @@ export class Camera extends React.Component<CameraProps, CameraState> {
     private video: HTMLVideoElement;
     private canvas: HTMLCanvasElement;
     private photo: HTMLPictureElement;
+    private canvasInterval: any;
 
     private userFacingMode = "user";
     private environmentFacingMode = "environment";
 
     constructor(props: any) {
         super(props);
+        this.applyFilter = this.applyFilter.bind(this);
+        this.removeFilter = this.removeFilter.bind(this);
         this.takeRegistrationPhoto = this.takeRegistrationPhoto.bind(this);
         this.paintToCanvas = this.paintToCanvas.bind(this);
+        this.paintCanvasUsingColour = this.paintCanvasUsingColour.bind(this);
         this.flipCamera = this.flipCamera.bind(this);
         this.state = {
             width: this.props.width,
             height: this.props.height,
             stream: undefined,
+            originalImage: undefined,
             facingMode: "",
             constraints: {},
             devices: [],
@@ -66,14 +72,33 @@ export class Camera extends React.Component<CameraProps, CameraState> {
                     <Grid verticalAlign="middle" columns={1} centered >
                         <Grid.Row>
                             <Segment className="camera">
-                                <video className="video" ref={(input) => { this.video = input; }}>
-                                    Video stream not available.
-                                </video>
+                                <canvas
+                                    className="canvas"
+                                    ref={(input) => { this.canvas = input; }}
+                                />
                             </Segment>
                         </Grid.Row>
                         <Grid.Row>
                             <Button className="button" onClick={this.takeRegistrationPhoto} icon="camera" />
                             <Button className="button" onClick={this.flipCamera}>Flip Camera</Button>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Form>
+                                <Grid container columns={4} stackable className="grid">
+                                    <Grid.Column>
+                                        <Button className="button" onClick={() => { this.applyFilter("red" ); }}>Red</Button>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Button className="button" onClick={() => { this.applyFilter("green"); }}>Green</Button>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Button className="button" onClick={() => { this.applyFilter("blue"); }}>Blue</Button>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <Button className="button" onClick={this.removeFilter}>No filter</Button>
+                                    </Grid.Column>
+                                </Grid>
+                            </Form>
                         </Grid.Row>
                     </Grid>
                 </Grid.Column>
@@ -86,10 +111,9 @@ export class Camera extends React.Component<CameraProps, CameraState> {
                     />
                     </Segment>
                 </Grid.Column>
-                <canvas
-                    className="canvas"
-                    ref={(input) => { this.canvas = input; }}
-                />
+                <video className="video" ref={(input) => { this.video = input; }}>
+                    Video stream not available.
+                </video>
             </Grid >
         );
     }
@@ -148,8 +172,37 @@ export class Camera extends React.Component<CameraProps, CameraState> {
         this.canvas.width = this.state.width;
         this.canvas.height = this.state.height;
         const context = this.canvas.getContext("2d");
-        return setInterval(() => {
+        this.setState({
+            originalImage: context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        });
+        this.canvasInterval = setInterval(() => {
             context.drawImage(video, 0, 0, this.canvas.width, this.canvas.height);
+        }, 16);
+    }
+
+    private removeFilter() {
+        window.clearInterval(this.canvasInterval);
+        const context = this.canvas.getContext("2d");
+        context.putImageData(this.state.originalImage, 0, 0);
+        this.canvasInterval = setInterval(() => {
+            context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+        }, 16);
+    }
+
+    private applyFilter(filterColour: string) {
+        this.paintCanvasUsingColour(filterColour, this.video);
+    }
+
+    private paintCanvasUsingColour(colour: string, video: HTMLVideoElement) {
+        window.clearInterval(this.canvasInterval);
+        this.canvas.width = this.state.width;
+        this.canvas.height = this.state.height;
+        const context = this.canvas.getContext("2d");
+        this.canvasInterval = setInterval(() => {
+            context.drawImage(video, 0, 0, this.canvas.width, this.canvas.height);
+            let pixels = context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+            pixels = colourEffect(pixels, colour);
+            context.putImageData(pixels, 0, 0);
         }, 16);
     }
 
