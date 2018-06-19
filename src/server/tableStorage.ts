23a163748d80061b5bdd24772efedd10a5864cb5
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-06-19 15:16:04 +0100

    check login data

--- src/server/tableStorage.ts
+++ src/server/tableStorage.ts
@@ -1,6 +1,8 @@
 import * as storage from "azure-storage";
 import * as uuid from "uuid";
 import { UserDetails, TableTask } from "./models";
+import { userInfo } from "os";
+import { retrieveBlobUrl } from "./blobStorage";
 
 const devStorageCredentials = storage.generateDevelopmentStorageCredentials();
 
@@ -34,5 +36,25 @@ export const uploadUser = async (userDetails: UserDetails) => {
         } else {
             console.log(`Added ${task.username} to the ${tableName} table`);
         }
+    });
+};
+
+export const checkUserExists = async (userDetails: UserDetails) => {
+    const query = new storage.TableQuery()
+        .select()
+        .where("name eq ?string? and username eq ?string?", userDetails.name, userDetails.username);
+    return new Promise((resolve, reject) => {
+        blobService.queryEntities(tableName, query, null, async (error, queryResult) => {
+            if (error) {
+                reject(error);
+            } else {
+                try {
+                    resolve(queryResult.entries.length);
+                } catch (err) {
+                    console.error(err);
+                    reject(err);
+                }
+            }
+        });
     });
 };
