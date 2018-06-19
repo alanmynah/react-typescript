940974ff28f00981056543bf61c8e1a41c34f9bf
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-06-19 10:15:01 +0100

    reroute

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import {Container} from "semantic-ui-react";
-import { ThankYou } from "./components/ThankYou";
-import { Footer } from "./components/Shared/Footer";
 import { Header } from "./components/Shared/Header";
+import { Footer } from "./components/Shared/Footer";
+import { ThankYou } from "./components/Forms/ThankYou";
 import Registration from "./components/Forms/Registration";
+import Login from "./components/Forms/Login";
+import { LoginAttemptsList } from "./components/List/List";
 
 export class App extends React.Component {
 
@@ -14,9 +16,13 @@ export class App extends React.Component {
                 <div>
                     <Header />
                     <Container text style={{ marginTop: "7em" }}>
-                        <Redirect path="/" to="/registration" />
-                        <Route path="/registration" component={Registration}/>
-                        <Route path="/thankyou" component={ThankYou}/>
+                        <Switch>
+                            <Redirect exact={true} path="/" to="/login" />
+                            <Route path="/login" component={Login} />
+                            <Route path="/registration" component={Registration}/>
+                            <Route path="/thankyou" component={ThankYou}/>
+                            <Route path="/list" component={LoginAttemptsList} />
+                        </Switch>
                     </Container>
                     <Footer />
                 </div>
